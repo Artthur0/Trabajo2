d1be49316d9da2d7ee8f4d4fb8b6d2ddad53a486
[Arthur] <115912843+Artthur0@users.noreply.github.com>
2024-10-26 19:22:13 -0300

    Creación de forms y clases a usar, futura recopilacion de datos

---
+++ Trabajo2/Clases/Asignatura.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabajo2.Clases
+{
+    public class Asignatura
+    { 
+        public int CodigoAsignatura {  get; set; }
+        public string NombreAsig { get; set; }
+        public int Creditos { get; set; }
+
+
+    }
+}
---
+++ Trabajo2/FAlumno.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trabajo2
+{
+    public partial class FAlumno : Form
+    {
+        public FAlumno()
+        {
+            InitializeComponent();
+        }
+    }
+}
